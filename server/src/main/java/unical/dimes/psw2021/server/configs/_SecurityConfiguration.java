ec490cf2a9887b3929aba423d741f5a4887fbf5c
Stefano <pernastefano99@gmail.com>
2021-06-08 12:09:24 +0200

    commit

---
+++ server/src/main/java/unical/dimes/psw2021/server/configs/_SecurityConfiguration.java
@@ -0,0 +1,93 @@
+//package unical.dimes.psw2021.server.configs;
+//
+//
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+//import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+//import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+//import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+//import org.springframework.security.core.session.SessionRegistryImpl;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+//import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+//import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+//import unical.dimes.psw2021.server.support.authentication.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.convert.converter.Converter;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AbstractAuthenticationToken;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.oauth2.jwt.Jwt;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+//public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+//    @Value("${base-url}") private String baseUrl;
+//
+//
+//    @Autowired
+//    public void configureGlobal(
+//            AuthenticationManagerBuilder auth) throws Exception {
+//
+//        KeycloakAuthenticationProvider keycloakAuthenticationProvider
+//                = keycloakAuthenticationProvider();
+//        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
+//                new SimpleAuthorityMapper());
+//        auth.authenticationProvider(keycloakAuthenticationProvider);
+//    }
+//
+//    @Bean
+//    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+//        return new KeycloakSpringBootConfigResolver();
+//    }
+//
+//    @Bean
+//    @Override
+//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+//        return new RegisterSessionAuthenticationStrategy(
+//                new SessionRegistryImpl());
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.authorizeRequests()
+//                .antMatchers(baseUrl+"/search/**").permitAll()
+//                .antMatchers(baseUrl+"/users/**").permitAll()
+//                .antMatchers(baseUrl+"/booking/**").permitAll()
+//                .antMatchers(baseUrl+"/restaurants/**").permitAll()
+//                .antMatchers("/v3/api-docs/**").permitAll()
+//                .antMatchers("/swagger-ui/**").permitAll()
+//                .anyRequest()
+//                .authenticated().and().oauth2ResourceServer().jwt()
+//                .jwtAuthenticationConverter(new JwtAuthenticationConverter());
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowCredentials(true);
+//        configuration.addAllowedOriginPattern("*");
+//        configuration.addAllowedHeader("*");
+//        configuration.addAllowedMethod("OPTIONS");
+//        configuration.addAllowedMethod("GET");
+//        configuration.addAllowedMethod("POST");
+//        configuration.addAllowedMethod("PUT");
+//        source.registerCorsConfiguration("/**", configuration);
+//        return new CorsFilter(source);
+//    }
+//}
